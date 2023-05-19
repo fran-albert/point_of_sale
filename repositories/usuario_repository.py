7e5cb85442cfd3f7dde961ce749559769c69bcae
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-05-19 18:21:10 -0300

    Revert "Cambio de nombre de la entidad Usuario a Vendedor. Metodos GET - SET"
    This reverts commit 47845b2f16e6be1e3822c4fce3beb370e3fe3f67.

--- repositories/usuario_repository.py
+++ repositories/usuario_repository.py
@@ -1,5 +1,5 @@
 from mysqlx import Error
-from entities.vendedor import Vendedor
+from entities.usuario import Usuario
 from access.mysql_connection import MySQLConnection
 
 class UsuarioRepository:
@@ -28,7 +28,7 @@ class UsuarioRepository:
                     id = row[0]
                     username = row[1]
                     password = row[2]
-                    nuevoUsuario = Vendedor(id, username, password)
+                    nuevoUsuario = Usuario(id, username, password)
                     usuarios.append(nuevoUsuario)
             return usuarios
         except Error as e:
